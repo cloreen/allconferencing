20cd7436265706ed8d8a6854222d4f1eabd36db9
cloreen <cloreen@hotmail.com>
2015-08-16 12:11:15 -0700

    DialOutTest without endMeeting and driver.quit methods

--- src/test/java/com/lotus/allconferencing/meeting_controller/DialOutTest.java
+++ src/test/java/com/lotus/allconferencing/meeting_controller/DialOutTest.java
@@ -5,7 +5,11 @@ import com.lotus.allconferencing.webdriver.manager.Driver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +20,7 @@ import java.util.Set;
  */
 public class DialOutTest {
     private WebDriver driver;
-    private String baseWindow, myAccountWindow;
+    private String baseWindow, myAccountWindow, projectManagerWindow, meetingControllerWindow;
     private LoginPageObject loginPage;
 
     public String getWindow() {
@@ -47,21 +51,87 @@ public class DialOutTest {
 
     }
 
+    // Sets up an Instant Meeting from Account Landing page and sends an invite based on argument
+    // Arguments -
+    //      1. Invite from Instant Meeting Setup interface in Project Manager
+    //      2. Invite from within Meeting Controller after meeting has started
+
+    public void startInstantMeeting() {
+        WebElement instMtgLink = driver.findElement(By.cssSelector("a[id='lnkDialIn']"));
+        instMtgLink.click();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Switch to newly opened window
+        projectManagerWindow = getWindow();
+        System.out.println("Project Manager window handle is: " + projectManagerWindow);
+
+        WebElement newBaseElement = driver.findElement(By.cssSelector("html"));
+        newBaseElement.click();
+
+        WebElement confirmButton = driver.findElement(By.cssSelector("button[id='amndstart-button']"));
+        confirmButton.click();
+        confirmButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(
+                ExpectedConditions.textToBePresentInElement(confirmButton, "Start Meeting Now")
+        );
+
+        confirmButton.click();
+
+        // Switch to newly opened window
+        meetingControllerWindow = getWindow();
+        System.out.println("Meeting Controller window handle is: " + meetingControllerWindow);
+
+        WebDriverWait waitForMtgCntrlr = new WebDriverWait(driver, 10);
+        waitForMtgCntrlr.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[8]/div/div[2]/div[2]/div/div/div/ul/li")));
+
+    }
+
+
     @Before
     public void setup() {
         Driver.set(Driver.BrowserName.FIREFOX);
         driver = Driver.get("http://www.allconferencing.com/");
         baseWindow = driver.getWindowHandle();
         getLoginPage(LoginPageObject.LoginType.STANDARD);
+        startInstantMeeting();
     }
 
     @Test
     public void dialOut() {
-        // do nothing
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement currentHeaderElement;
+
+        WebElement topMenuBarDialPadOption = driver.findElement(By.xpath("/html/body/div[7]/div[2]/div/table/tbody/tr/td[2]/div/div/ul/li[3]/a"));
+        topMenuBarDialPadOption.click();
+
+        WebElement dialPadInput = driver.findElement(By.cssSelector("input[id='numberbox']"));
+        dialPadInput.sendKeys(new String("3102103647"));
+        WebElement dialPadCallButton = driver.findElement(By.cssSelector("img[id='buttonCall']"));
+        dialPadCallButton.click();
+        wait.until(ExpectedConditions.textToBePresentInElement(currentHeaderElement = driver.findElement(By.xpath("/html/body/div[4]/div/div")), "Dial-out"));
+
+        WebElement dialOutButton = driver.findElement(By.cssSelector("button[id='dialoutbutton-button']"));
+        dialOutButton.click();
+        wait.until(ExpectedConditions.textToBePresentInElement(currentHeaderElement = driver.findElement(By.xpath(".//*[@id='simpleerrordialog']/div[1]")/*("/html/body/div[6]/div[1]/div[1]")*/), "Dial-out Response"));
+
+        WebElement dialOutDialogButton = driver.findElement(By.cssSelector("button[id='simpleerrordialogok-button']"));
+        dialOutDialogButton.click();
+
+        /***************************************************************************************************************
+        * Manually check for call to be received.
+        */
+
     }
 
     @After
     public void tearDown() {
-        driver.quit();
+        //driver.quit();
     }
 }
