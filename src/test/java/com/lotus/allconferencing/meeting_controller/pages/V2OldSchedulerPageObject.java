d41bd11078a84f6ad10e0bae1b8ac4c8eda5d0d1
cloreen <cloreen@hotmail.com>
2015-12-08 22:00:02 -0800

    Refactored conference time setup to method in Page Object

---
+++ src/test/java/com/lotus/allconferencing/meeting_controller/pages/V2OldSchedulerPageObject.java
@@ -0,0 +1,158 @@
+package com.lotus.allconferencing.meeting_controller.pages;
+
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.lotus.allconferencing.BaseSeleniumTest;
+import com.lotus.allconferencing.ReadPropertyFile;
+import com.lotus.allconferencing.meeting_controller.pages.components.GmailInboxComponentsObject;
+import com.lotus.allconferencing.meeting_controller.pages.components.GmailLoginPageObject;
+import com.lotus.allconferencing.meeting_controller.pages.components.V2OldSchedulerComponents;
+import org.joda.time.DateTime;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Ben on 7/20/2015.
+ */
+
+/************
+ * TODO - Comment refactor points
+ */
+public class V2OldSchedulerPageObject extends BaseSeleniumTest {
+    private static WebDriver driver;
+    private ReadPropertyFile readProps = null;
+
+
+
+
+    public V2OldSchedulerPageObject(WebDriver newDriver) {
+
+        driver = newDriver;
+
+        try {
+            readProps = new ReadPropertyFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public V2OldSchedulerComponents v2OldSchedulerComponents = new V2OldSchedulerComponents(driver);
+
+    public void choosePacificTimeZone() {
+        //Select timeZoneSelect = new Select(driver.findElement(By.cssSelector("select[name='cboTimeZone']")));
+        WebElement timeZoneElement = driver.findElement(By.cssSelector("select[name='cboTimeZone']"));
+        Select timeZoneSelect = new Select(timeZoneElement);
+        List<WebElement> timeZoneOptions = timeZoneSelect.getOptions();
+        int timeZoneOptionsIteration = 0;
+        for (WebElement option : timeZoneOptions) {
+            timeZoneOptionsIteration++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (timeZoneOptionsIteration == 4) { // Option evaluation as String does not work here, so click on option in 4th iteration of loop.
+                option.click();
+                break;
+            } else if (timeZoneOptionsIteration == timeZoneOptions.size()) {
+                System.out.println("System never found correct option.");
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+
+    public void addParticipant() {
+        WebElement participantEmailField = v2OldSchedulerComponents.getParticipantEmailField();
+        WebElement addToMeetingButton = v2OldSchedulerComponents.getAddToMeetingButton();
+        participantEmailField.sendKeys(readProps.getParticipantEmail());
+        addToMeetingButton.click();
+    }
+
+    public void enableEmailReminders() {
+        WebElement sendReminderCheckbox = v2OldSchedulerComponents.getSendReminderCheckbox();
+        sendReminderCheckbox.click();
+    }
+
+    public void submitForm() {
+        WebElement submitButton = v2OldSchedulerComponents.getSubmitButton();
+        submitButton.click();
+    }
+
+    public void goToAccountServices() {
+        WebElement acctSvcsButton = v2OldSchedulerComponents.getAcctSvcsButton();
+        acctSvcsButton.click();
+        WebDriverWait waitForAcctSvcs = new WebDriverWait(driver, 10);
+        waitForAcctSvcs.until(
+                ExpectedConditions.titleIs("All Conferencing - Account Services")
+        );
+    }
+
+    public void inputTime() {
+        // Check specific meeting time (rather than immediate
+        WebElement specifyTimeRadioButton = driver.findElement(By.cssSelector("input[name='Rule_Type'][value='adhoc']"));
+        specifyTimeRadioButton.click();
+
+        // Choose proper meeting time
+        Select meetingHourSelect = new Select(driver.findElement(By.cssSelector("select[name='Rule_Start_Hour']")));
+        List<WebElement> meetingHourOptions = meetingHourSelect.getOptions();
+        DateTime currentTime = new DateTime();
+        Integer currentHour = currentTime.getHourOfDay();
+        System.out.println("Current Hour is: " + currentHour);
+        Integer meetingHour = 0;
+        String timeOfDay = "";
+        if (currentHour <= 11 || currentHour == 23) {
+            timeOfDay = "AM";
+        } else {
+            timeOfDay = "PM";
+        }
+        if (currentHour > 12) {
+            currentHour -= 12;
+        }
+        if (currentHour == 12) {
+            meetingHour = 1;
+        } else {
+            meetingHour = currentHour + 1;
+        }
+        int selectOptionsIteration = 0;
+        for (WebElement option : meetingHourOptions) {
+            selectOptionsIteration++;
+            Integer optionValue = Integer.parseInt(option.getAttribute("value"));
+            if (optionValue == meetingHour) {
+                option.click();
+                break;
+            } else if (selectOptionsIteration == meetingHourOptions.size()) {
+                System.out.println("System never found correct option.");
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Choose AM or PM
+        Select timeOfDaySelect = new Select(driver.findElement(By.cssSelector("select[name='Rule_Start_AM']")));
+        List<WebElement> timeOfDaySelectOptions = timeOfDaySelect.getOptions();
+        int timeOfDaySelectOptionsIteration = 0;
+        for (WebElement option : timeOfDaySelectOptions) {
+            timeOfDaySelectOptionsIteration++;
+            if (option.getAttribute("value") == timeOfDay) {
+                option.click();
+                break;
+            }
+        }
+    }
+}
