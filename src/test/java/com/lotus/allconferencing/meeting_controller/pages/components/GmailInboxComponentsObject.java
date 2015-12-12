e3e61e1b1f24c806b911e59a7903bb8ba637124c
cloreen <cloreen@hotmail.com>
2015-12-11 19:41:15 -0800

    Refactored  getEmailSubject method in Components Class to use static By for locator

--- src/test/java/com/lotus/allconferencing/meeting_controller/pages/components/GmailInboxComponentsObject.java
+++ src/test/java/com/lotus/allconferencing/meeting_controller/pages/components/GmailInboxComponentsObject.java
@@ -17,8 +17,9 @@ public class GmailInboxComponentsObject {
     private ReadPropertyFile readProps = null;
 
     // Selectors for Gmail Inbox Page Components--------------------------------------------
-    private static final By REFRESH = By.xpath("/html/body/div[7]/div[3]/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div/div/div[4]/div");
+    private static final By EMAIL_SUBJECT = By.cssSelector("table[id=':36'] tbody tr td:nth-of-type(6) div div div span");
     private static final By TIMESTAMP = By.cssSelector("table[id=':36'] tbody tr td:nth-of-type(8) span");
+    private static final By REFRESH = By.xpath("/html/body/div[7]/div[3]/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div/div/div[4]/div");
     //-----------------------------------------------------------------------------------------------
 
 
@@ -35,7 +36,7 @@ public class GmailInboxComponentsObject {
     }
 
     public WebElement getEmailSubject() {
-        WebElement emailSubject = driver.findElement(By.cssSelector("table[id=':36'] tbody tr td:nth-of-type(6) div div div span"));
+        WebElement emailSubject = driver.findElement(EMAIL_SUBJECT);
         return emailSubject;
     }
 
