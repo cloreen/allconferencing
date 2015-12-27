99a5ea047f52cb5506ed89c1d387c63a0c501a57
cloreen <cloreen@hotmail.com>
2015-12-27 11:46:11 -0800

    Fixed getWindow() method to properly receive driver and return string

--- src/main/java/com/lotus/allconferencing/webdriver/manager/WindowManager.java
+++ src/main/java/com/lotus/allconferencing/webdriver/manager/WindowManager.java
@@ -9,29 +9,21 @@ import java.util.Set;
 /**
  * Created by Ben on 8/9/2015.
  */
-public class WindowManager extends Page {
-    public WebDriver driver;
+public class WindowManager {
+    private static WebDriver driver;
 
-    public WindowManager(WebDriver driver) {
-        super(driver);
+    public WindowManager(WebDriver newDriver) {
+
+        driver = newDriver;
+
     }
 
 
     public String getWindow() {
         int i = 0;
         String windowHandle = "";
-/*
-        Set<String> winSet = inSet; //driver.getWindowHandles();
-        for (String item : inSet) {
-            System.out.println("Handle is: " + item);
-        }
-        List<String> windowHandles = new ArrayList<String>();
-        for (String item : winSet) {
-            driver.switchTo().window(item);
-        }
-*/
+
         Set<String> handlesSet = driver.getWindowHandles();
-        List<String> windowHandles = new ArrayList<String>();
         for (String item : handlesSet) {
             driver.switchTo().window(item);
         }
