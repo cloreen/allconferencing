ea450d139a0c08abd92fe9db3a4318f5388df0b5
cloreen <cloreen@hotmail.com>
2016-01-11 22:09:08 -0800

    Created class

---
+++ src/test/java/com/lotus/allconferencing/services/pages/CorpAccountServicesPage.java
@@ -0,0 +1,36 @@
+package com.lotus.allconferencing.services.pages;
+
+import com.lotus.allconferencing.ReadPropertyFile;
+import com.lotus.allconferencing.services.components.SimpleAccountServicesComponents;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Ben on 12/17/2015.
+ */
+public class CorpAccountServicesPage {
+    private static WebDriver driver;
+    private ReadPropertyFile readProps = null;
+
+    public CorpAccountServicesPage(WebDriver newDriver) {
+        driver = newDriver;
+
+        try {
+            readProps = new ReadPropertyFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openEasyAllInvitePage() {
+        SimpleAccountServicesComponents simpleAccountServicesComponents = new SimpleAccountServicesComponents(driver);
+        WebElement easyAllInvite = simpleAccountServicesComponents.getEasyAllInviteLink();
+        easyAllInvite.click();
+        WebDriverWait waitForInvitePage = new WebDriverWait(driver, 10);
+        waitForInvitePage.until(
+                ExpectedConditions.titleIs(simpleAccountServicesComponents.getExpectedEasyAllTitle())
+        );
+    }
+}
