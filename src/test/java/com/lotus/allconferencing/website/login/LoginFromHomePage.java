112191f5823480320a547ab55fbd8060b08e4a29
cloreen <cloreen@hotmail.com>
2016-01-23 20:34:57 -0800

    Removed unused variable and page declarations

--- src/test/java/com/lotus/allconferencing/website/login/LoginFromHomePage.java
+++ src/test/java/com/lotus/allconferencing/website/login/LoginFromHomePage.java
@@ -1,11 +1,8 @@
 package com.lotus.allconferencing.website.login;
 
 import com.lotus.allconferencing.ReadPropertyFile;
-import com.lotus.allconferencing.services.components.CorpAccountServicesComponents;
-import com.lotus.allconferencing.services.components.OldAccountServicesComponents;
 import com.lotus.allconferencing.services.pages.CorpAccountServicesPage;
 import com.lotus.allconferencing.services.pages.OldAccountServicesPage;
-import com.lotus.allconferencing.services.participantprojectshare.components.PartProjectShareComponents;
 import com.lotus.allconferencing.services.participantprojectshare.pages.PartProjectSharePage;
 import com.lotus.allconferencing.website.login.pages.AccountType;
 import com.lotus.allconferencing.website.login.pages.LoginPageObject;
@@ -15,12 +12,8 @@ import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -35,22 +28,11 @@ public class LoginFromHomePage {
     private static LoginPageObject.AccessType accessType;
     private static AccountType.AcctType accountType;
     private static AccountType.LoginType loginType;
-    private static String currentPage, loginPageTitle, acctLandingPageTitle = "";
-    private static String baseWindow, myAccountWindow;
-    private static String homeURL = "http://www.allconferencing.com";
-    private static WebElement acctSvcsTitle;
-    private static String pageTitle;
-    private static List<WebElement> webElements;
-    private static int webElementsSize;
-    private static By locator;
 
     private HomePage homePage = new HomePage(driver);
     private OldAccountServicesPage oldAccountServicesPage = new OldAccountServicesPage(driver);
-    private OldAccountServicesComponents oldAccountServicesComponents = new OldAccountServicesComponents(driver);
     private CorpAccountServicesPage corpAccountServicesPage = new CorpAccountServicesPage(driver);
-    private CorpAccountServicesComponents corpAccountServicesComponents = new CorpAccountServicesComponents(driver);
     private PartProjectSharePage partProjectSharePage = new PartProjectSharePage(driver);
-    private PartProjectShareComponents partProjectShareComponents = new PartProjectShareComponents(driver);
 
     @BeforeClass
     public static void setup() {
