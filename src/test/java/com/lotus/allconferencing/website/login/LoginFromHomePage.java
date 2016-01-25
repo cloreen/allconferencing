1cc6c7910d7a348bb4ee86568a6b71d06e66257b
cloreen <cloreen@hotmail.com>
2016-01-24 20:10:38 -0800

    Added adversity tests to cover all possible login exception scenarios

--- src/test/java/com/lotus/allconferencing/website/login/LoginFromHomePage.java
+++ src/test/java/com/lotus/allconferencing/website/login/LoginFromHomePage.java
@@ -16,6 +16,7 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by Ben on 5/1/2015.
@@ -30,6 +31,7 @@ public class LoginFromHomePage extends BaseSeleniumTest {
     private static AccountType.LoginType loginType;
 
     private HomePage homePage = new HomePage(driver);
+    private LoginPageObject loginPage = new LoginPageObject(driver);
     private OldAccountServicesPage oldAccountServicesPage = new OldAccountServicesPage(driver);
     private CorpAccountServicesPage corpAccountServicesPage = new CorpAccountServicesPage(driver);
     private PartProjectSharePage partProjectSharePage = new PartProjectSharePage(driver);
@@ -55,7 +57,31 @@ public class LoginFromHomePage extends BaseSeleniumTest {
     }
 
     @Test
-    public void test02_CorpLogin() {
+    public void test02_StandardLoginNoClientID() {
+        homePage.login(loginType.STANDARD, accountType.STANDARD_OLD, accessType.NOCLIENTID);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test03_StandardLoginNoPass() {
+        homePage.login(loginType.STANDARD, accountType.STANDARD_OLD, accessType.NOPASS);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test04_StandardLoginWrongClientID() {
+        homePage.login(loginType.STANDARD, accountType.STANDARD_OLD, accessType.INVALIDCLIENTID);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test05_StandardLoginWrongPass() {
+        homePage.login(loginType.STANDARD, accountType.STANDARD_OLD, accessType.INVALIDPASS);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test06_CorpLogin() {
         homePage.login(loginType.CORPORATE, accountType.CORPORATE, accessType.LOGIN);
         corpAccountServicesPage.logout();
         LoginPageObject loginPage = new LoginPageObject(driver, loginType.CORPORATE, accountType.CORPORATE, accessType.DISPLAY);
@@ -63,10 +89,46 @@ public class LoginFromHomePage extends BaseSeleniumTest {
     }
 
     @Test
-    public void test03_ParticipantLogin() {
+    public void test07_CorpLoginNoClientID() {
+        homePage.login(loginType.CORPORATE, accountType.CORPORATE, accessType.NOCLIENTID);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.CORP_LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test08_CorpLoginNoPassword() {
+        homePage.login(loginType.CORPORATE, accountType.CORPORATE, accessType.NOPASS);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.CORP_LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test09_CorpLoginWrongClientID() {
+        homePage.login(loginType.CORPORATE, accountType.CORPORATE, accessType.INVALIDCLIENTID);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.CORP_LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test10_CorpLoginWrongPassword() {
+        homePage.login(loginType.CORPORATE, accountType.CORPORATE, accessType.INVALIDPASS);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.CORP_LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test11_ParticipantLogin() {
         homePage.login(loginType.PARTICIPANT, accountType.PARTICIPANT, accessType.LOGIN);
         partProjectSharePage.logout();
         assertEquals("Title is as expected", homePage.EXPECTED_TITLE, driver.getTitle());
+    }
+
+    @Test
+    public void test12_ParticipantLoginWrongClientID() {
+        homePage.login(loginType.PARTICIPANT, accountType.PARTICIPANT, accessType.INVALIDCLIENTID);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.LOGIN_EXPECTED_TITLE));
+    }
+
+    @Test
+    public void test13_ParticipantLoginWrongPassword() {
+        homePage.login(loginType.PARTICIPANT, accountType.PARTICIPANT, accessType.INVALIDPASS);
+        assertTrue("Login is unsuccessful", driver.getTitle().equals(loginPage.LOGIN_EXPECTED_TITLE));
     }
 
     @AfterClass
