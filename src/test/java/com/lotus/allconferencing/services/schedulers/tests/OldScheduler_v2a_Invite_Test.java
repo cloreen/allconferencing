452f49ed77b72c79fc6db17247c2c2b11a392a88
cloreen <cloreen@hotmail.com>
2016-03-18 17:48:35 -0700

    Created v2a old scheduler invite test class from v1a copy, changed it to take excel data from standalone excel data class.

--- src/test/java/com/lotus/allconferencing/services/schedulers/tests/OldScheduler_v2a_Invite_Test.java
+++ src/test/java/com/lotus/allconferencing/services/schedulers/tests/OldScheduler_v2a_Invite_Test.java
@@ -1,13 +1,12 @@
 package com.lotus.allconferencing.services.schedulers.tests;
 
 import com.lotus.allconferencing.BaseSeleniumTest;
+import com.lotus.allconferencing.ExcelData;
 import com.lotus.allconferencing.ReadPropertyFile;
 import com.lotus.allconferencing.services.pages.ConferenceListPage;
 import com.lotus.allconferencing.services.pages.OldAccountServicesPage;
 import com.lotus.allconferencing.services.schedulers.pages.OldSchedulerPageObject;
 import com.lotus.allconferencing.support_classes.GmailObject;
-import com.lotus.allconferencing.website.login.pages.AccountType;
-import com.lotus.allconferencing.website.login.pages.LoginPageObject;
 import com.lotus.allconferencing.website.pages.HomePage;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -16,11 +15,6 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -28,16 +22,9 @@ public class OldScheduler_v2a_Invite_Test extends BaseSeleniumTest {
     private static WebDriver inDriver;
     private static WebDriver driver;
     private static WebDriver driver2;
-    private static Collection<Object[]> excelData;
-    private static ArrayList<Object[]> dataArray;
-    private static Object[] excelObjectArray;
-    private static Object excelObject;
-    private static List<Object> dataList;
-    private LoginPageObject loginPage;
-//    private LoginPageObject.AccessType accessType;
+
     private static ReadPropertyFile readProps = null;
     private static String baseWindow;
-    private static String myAccountWindow;
     private static Boolean conferenceDisplays = false;
     private static String partPasscode = "";
     private static String timeOfDay = "";
@@ -48,31 +35,25 @@ public class OldScheduler_v2a_Invite_Test extends BaseSeleniumTest {
     OldSchedulerPageObject oldScheduler = new  OldSchedulerPageObject(driver);
     OldAccountServicesPage oldAccountServicesPage = new OldAccountServicesPage(driver);
     ConferenceListPage conferenceListPage = new ConferenceListPage(driver);
-
-    static AccountType.LoginType loginType;
-    static AccountType.AcctType accountType;
-    static LoginPageObject.AccessType accessType;
-    static Browser browser;
-
-    public enum Browser {FIREFOX, CHROME, IE, OPERA, SAUCELABS, HTMLUNIT}
+    static ExcelData excelData = new ExcelData();
 
     @BeforeClass
     public static void setup() {
         readProps = getSettings();
-        getDataFromExcel();
-        driver = openBrowser(browser);
+        ExcelData.getDataFromExcel();
+        driver = openBrowser(ExcelData.browser);
     }
 
     @Test
     public void scheduleMeetingAndCheckPasscodes() {
         //Login and schedule meeting
 //        homePage.login(AccountType.LoginType.STANDARD, AccountType.AcctType.STANDARD_OLD, LoginPageObject.AccessType.LOGIN);
-        homePage.login(loginType, accountType, accessType);
+        homePage.login(ExcelData.loginType, ExcelData.accountType, ExcelData.accessType);
         oldAccountServicesPage.openScheduler(version);
         oldScheduler.setupMeeting(timeOfDay, version);
 
         //Get passcode from email
-        driver2 = openBrowser(browser);
+        driver2 = openBrowser(ExcelData.browser);
         partPasscode = getParticipantPasscodeFromEmail();
         closeEmailWindow();
 
@@ -105,91 +86,10 @@ public class OldScheduler_v2a_Invite_Test extends BaseSeleniumTest {
 
     // Helper methods --------------------------------------------------------------------------------------------------
 
-    public static WebDriver openBrowser(Browser inBrowser) {
+    public static WebDriver openBrowser(ExcelData.Browser inBrowser) {
         String browser = inBrowser.toString();
         inDriver = setDriver(BrowserName.valueOf(browser));
         return inDriver;
-    }
-
-    public static void getDataFromExcel() {
-        try {
-            excelData = dataFromExcel();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        dataArray = (ArrayList<Object[]>)excelData;
-        for(Object[] objectArray : dataArray) {
-            evaluateExcelData(objectArray);
-        }
-    }
-
-    private static void evaluateExcelData(Object[] inObjArray) {
-        int i = 0;
-        for(Object object : inObjArray) {
-            System.out.println("Object " + object + " in objectArray from Excel sheet is: " + object.toString());
-            switch(i) {
-                case 0:
-                    switch(object.toString()) {
-                        case "FIREFOX":
-                            browser = Browser.FIREFOX;
-                            break;
-                        case "CHROME":
-                            browser = Browser.CHROME;
-                            break;
-                        case "IE":
-                            browser = Browser.IE;
-                            break;
-                        case "OPERA":
-                            browser = Browser.OPERA;
-                            break;
-                        case "SAUCELABS":
-                            browser = Browser.SAUCELABS;
-                            break;
-                        case "HTMLUNIT":
-                            browser = Browser.HTMLUNIT;
-                            break;
-                    }
-                case 1:
-                    switch(object.toString()) {
-                        case "STANDARD":
-                            loginType = AccountType.LoginType.STANDARD;
-                            break;
-                        case "CORPORATE":
-                            loginType = AccountType.LoginType.CORPORATE;
-                            break;
-                        case "PARTICIPANT":
-                            loginType = AccountType.LoginType.PARTICIPANT;
-                            break;
-                    }
-                    break;
-                case 2:
-                    switch(object.toString()) {
-                        case "STANDARD_OLD":
-                            accountType = AccountType.AcctType.STANDARD_OLD;
-                            break;
-                        case "STANDARD_NEW":
-                            accountType = AccountType.AcctType.STANDARD_NEW;
-                            break;
-                        case "STANDARD_SIMPLE":
-                            accountType = AccountType.AcctType.STANDARD_SIMPLE;
-                            break;
-                    }
-                    break;
-                case 3:
-                    switch (object.toString()) {
-                        case "LOGIN":
-                            accessType = LoginPageObject.AccessType.LOGIN;
-                    }
-                    break;
-                case 4:
-                    LoginPageObject.clientID = object.toString();
-                    break;
-                case 5:
-                    LoginPageObject.clientPass = object.toString();
-                    break;
-            }
-            i++;
-        }
     }
 
     // checkEmailIsReceived(), checkInviteEmail(), and checkEmailContentForNewInfo should be condensed into one method.
